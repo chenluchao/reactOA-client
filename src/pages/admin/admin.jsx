0390b9456c1f32fbc48df1fa9c25120b156fef28
clc <1972207371@qq.com>
2021-06-10 14:06:17 +0800

    :sparkles: 整体布局

--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -1,16 +1,29 @@
 import React, { Component } from 'react'
 import { Redirect } from 'react-router'
+import { Layout } from 'antd'
 import memoryUtils from '../../utils/memoryUtils'
+import LeftNav from '../../components/left-nav/left-nav'
+import Header from '../../components/header/header'
+const { Footer, Sider, Content } = Layout
 export default class Admin extends Component {
   render() {
-    const {user} = memoryUtils
-    if(!user._id){
-      return <Redirect to='/login'/>
+    const { user } = memoryUtils
+    if (!user._id) {
+      return <Redirect to="/login" />
     }
     return (
-      <div>
-        欢迎{user.username}
-      </div>
+      <Layout style={{ height: '100%' }}>
+        <Sider>
+          <LeftNav />
+        </Sider>
+        <Layout>
+          <Header>Header</Header>
+          <Content style={{ backgroundColor: '#fff' }}>Content</Content>
+          <Footer style={{ textAlign: 'center', color: '#aaa' }}>
+            建议使用Chrom浏览器进行浏览
+          </Footer>
+        </Layout>
+      </Layout>
     )
   }
 }
