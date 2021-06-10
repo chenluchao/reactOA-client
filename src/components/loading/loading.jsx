8dc555104f2ce142b2f6be810f9134f5c68922a0
clc <1972207371@qq.com>
2021-06-10 16:26:15 +0800

    :sparkles: 添加页面加载loading组件

--- src/components/loading/loading.jsx
+++ src/components/loading/loading.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-
+import { Spin } from 'antd'
 export default class Loading extends Component {
   render() {
     return (
-      <div>
-        加载中...
+      <div style={{padding:'200px 0',textAlign:'center'}}>
+        <Spin tip="加载中..." delay={300} size='large'/>
       </div>
     )
   }
