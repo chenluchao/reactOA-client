fc58af198e514d3cb5f3aa02a8ab578cbdc12dd7
clc <1972207371@qq.com>
2021-06-10 15:44:27 +0800

    :sparkles: 追加品类管理路由页面

---
+++ src/pages/products/category/category.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './index.less'
+export default class Category extends Component {
+  render() {
+    return (
+      <div>
+        Category
+      </div>
+    )
+  }
+}
