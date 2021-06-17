452fd17fd05fcc6344d0f9f03d1db6c06e4e7b31
clc <1972207371@qq.com>
2021-06-17 15:51:18 +0800

    :lipstick: 主展示区域滚动条样式修改

--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -1,6 +1,7 @@
 import React, { Component, lazy, Suspense } from 'react'
 import { Redirect, Route, Switch } from 'react-router-dom'
 import { Layout } from 'antd'
+import { Scrollbars } from 'react-custom-scrollbars'
 import memoryUtils from '../../utils/memoryUtils'
 import LeftNav from '../../components/left-nav/left-nav'
 import Header from '../../components/header/header'
@@ -27,23 +28,32 @@ export default class Admin extends Component {
           <LeftNav />
         </Sider>
         <Layout>
-          <Header/>
-          <Content style={{ backgroundColor: '#fff',margin:'20px 20px 0 20px',height:'100%',overflowY:'auto' }}>
-            <Suspense fallback={<Loading />}>
-              <Switch>
-                <Route path="/home" component={Home}></Route>
-                <Route path="/product" component={Products}></Route>
-                <Route path="/category" component={Category}></Route>
-                <Route path="/role" component={Role}></Route>
-                <Route path="/user" component={User}></Route>
-                <Route path="/charts/bar" component={Bar}></Route>
-                <Route path="/charts/line" component={Line}></Route>
-                <Route path="/charts/pie" component={Pie}></Route>
-                <Redirect to="/home"></Redirect>
-              </Switch>
-            </Suspense>
+          <Header />
+          <Content
+            style={{
+              backgroundColor: '#fff',
+              margin: '20px 20px 0 20px',
+              height: '100%',
+              overflow: 'hidden',
+            }}
+          >
+            <Scrollbars autoHide>
+              <Suspense fallback={<Loading />}>
+                <Switch>
+                  <Route path="/home" component={Home}></Route>
+                  <Route path="/product" component={Products}></Route>
+                  <Route path="/category" component={Category}></Route>
+                  <Route path="/role" component={Role}></Route>
+                  <Route path="/user" component={User}></Route>
+                  <Route path="/charts/bar" component={Bar}></Route>
+                  <Route path="/charts/line" component={Line}></Route>
+                  <Route path="/charts/pie" component={Pie}></Route>
+                  <Redirect to="/home"></Redirect>
+                </Switch>
+              </Suspense>
+            </Scrollbars>
           </Content>
-          <Footer style={{ textAlign: 'center', color: '#aaa'}}>
+          <Footer style={{ textAlign: 'center', color: '#aaa' }}>
             建议使用Chrom浏览器进行浏览
           </Footer>
         </Layout>
