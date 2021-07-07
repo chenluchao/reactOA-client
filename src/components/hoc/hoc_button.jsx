30b0256b32ee351c7df101e3a3f1e57d18712a92
clc <1972207371@qq.com>
2021-07-07 17:15:48 +0800

    :sparkles: 高阶组件引入redux判断按钮权限

--- src/components/hoc/hoc_button.jsx
+++ src/components/hoc/hoc_button.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types'
-
+// 引入store用于获取store中的数据
+import store from '../../redux/store'
 /**
  * 校验当前用户是否有功能编码对应的权限
  * @param {string} functionName
  * 参考：https://www.jianshu.com/p/26d605066187
  */
-const checkAuth=(functionName, menuId)=> {
+const checkAuth=(functionName)=> {
+  const currentUser = store.getState('user').user
   if (functionName) {
-    let functionsList;
-    if (localStorage.getItem('user_key')) {
-      functionsList = JSON.parse(localStorage.getItem('user_key'))
-    } else {
-      return false;
-    }
-    return functionsList.username==='admin';
+    return currentUser.username==='admin';
   } else {
     return false;
   }
@@ -25,7 +21,7 @@ const checkAuth=(functionName, menuId)=> {
  */
 export default class AuthWrapper extends React.Component {
   render() {
-    return checkAuth(this.props.functionName, this.props.menuId) && this.props.children;
+    return checkAuth(this.props.functionName) && this.props.children;
   }
 }
 
